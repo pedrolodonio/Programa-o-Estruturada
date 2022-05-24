0b34c3a912a0df05701d1a3b22450968e70553e4
pedrolodonio <99705704+pedrolodonio@users.noreply.github.com>
2022-05-24 17:55:19 -0300

    Update 1-Sistemas Eleitorais.c

--- 1-Sistemas Eleitorais.c
+++ 1-Sistemas Eleitorais.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-	int idade;
-		printf(">>>>Sistema Eleitoral<<<<\n");
-		printf("insira a idade: ");
-		scanf("%d",&idade);
-			if(idade<0){
-				printf("nao nasceu");
-			}
-			if(idade<=15){
-				printf ("nao vota");
-			}
-			if(idade<18){
-				printf("opicional");
-			}
-			if(idade>18){
-				printf("obrigatorio");
-			}
-			if(idade>64){
-				printf("opicional");
-			}
-			if(idade==41){
-				printf("ganhou premio 1");
-			}
-			if(idade==82){
-				printf("ganhou premio 2");
-			}
-			return 0;
-		}
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int idade = 0;
+	printf("eleitoral\n");
+	printf("digite a idade\n");
+	scanf("%d",&idade);
+	if(idade<1)
+		printf("nao nasceu");
+	else
+		if(idade>=1 && idade<=15)
+			printf("não vota");
+		else
+			if(idade==16|| idade==17)
+				printf("voto opicional");
+			else
+				if(idade==41)
+					printf("ganhou o premio 1 $$$");
+				else
+					if(idade==82)
+						printf("ganhou o premio 2 $$$");
+					else
+						if(idade>=18 && idade!=41 && idade<=64)
+							printf("voto obrigatorio");
+						else
+							if(idade>=65 && idade!=82)
+								printf("voto opicional");
+	return 0;
+}
