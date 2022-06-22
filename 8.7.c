be7ba1c83d0e2de3ae93d7467e01fad6f421bdf6
pedrolodonio <99705704+pedrolodonio@users.noreply.github.com>
2022-06-22 09:45:34 -0300

    Update 8.7.c

--- 8.7.c
+++ 8.7.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void troca(int *pa, int *pb) {
-int t; t=*pa; *pa=*pb; *pb=t;
-}
+void troca(int *pa, int *pb);
 
 int main(void){
-	int x[5]={0,1,2,3,4},*p=&x;
+	int x[5]={0,1,2,3,4};
+	int i;
 	
-	do{
-		troca(*p+1,*p+4);
-		printf("valor de x=%d",*p); 
-		*p++;	
-	}while(*p<5);
-
+	troca(x+1,x+4);
 	
-	
+	for(i=0;i<5;i++){
+		printf("valor de x=%d\n",x[i]); 
 	}
+}
+void troca(int *pa, int *pb) {
+	int t; 
+	t=*pa;
+	*pa=*pb;
+	*pb=t;
+}
